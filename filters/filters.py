f44068672ede77071ee6d4f1844b7ad686e96ac1
Andrey Gerasimov <gerasimov.andrey1985@gmail.com>
2023-11-17 22:06:42 +0400

    Доработаны фильтры для добавления слов и значений

--- filters/filters.py
+++ filters/filters.py
@@ -5,14 +5,16 @@ from re import fullmatch
 
 
 class IsLatinLetters(BaseFilter):
+    '''Этот фильтр отбирает тектст написанный латиницей, в котором слова разделены пробелом или тире'''
     async def __call__(self, message: Message) -> bool:
-        return message.text and all((i in (ascii_letters + ' ') for i in message.text))
+        pattern = r"^[a-zA-Z]+([ -][a-zA-Z]+)*$"
+        return message.text and bool(fullmatch(pattern, message.text))
 
 
 class IsListOfWords(BaseFilter):
     '''этот фильтр определяет является ли введённое сообщение списком слов написанных кириллицей, через запятую'''
     async def __call__(self, message: Message) -> bool:
-        pattern = r"^ *((([а-яА-Я])+|([а-яА-Я])+[ \-]([а-яА-Я])+) *, *)*(([а-яА-Я])+|([а-яА-Я])+[ \-]([а-яА-Я])+) *$"
+        pattern = r"^[а-яА-ЯёЁ]+([ ,\-] ?[а-яА-ЯёЁ]+)*$"
         return message.text and bool(fullmatch(pattern, message.text))
 
 
